6b1d8b8263601fc88e372ac044923bbb68b7c82d
unknown <khfong94@gmail.com>
2020-09-15 13:38:19 +0800

    fail the test case

--- src/stack/MyStringStack.java
+++ src/stack/MyStringStack.java
@@ -21,10 +21,6 @@ public class MyStringStack {
     private Vector<String> v = new Vector<String> ();
     public void push (String newElement) {
         if (newElement != null) v.add (newElement);
-
-        MyStringStack stack = new MyStringStack();
-        stack.push (s1);
-        assertTrue (stack.isEmpty());
     }
 
 /**
